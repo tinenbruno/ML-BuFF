63f93b3b18f34e769a7f8abb9f1d03d0ab920a7a
Bruno Tinen <Bruno Tinen>
2017-02-14 21:43:53 -0200

    Added getModel on baseFeatureRecord

--- ml_buff/models/base_feature_record.py
+++ ml_buff/models/base_feature_record.py
@@ -1,8 +1,15 @@
+from . import feature
+from ml_buff.database import Session
+
 class FeatureMeta(type):
     def __init__(cls, name, bases, dct):
         super(FeatureMeta, cls).__init__(name, bases, dct)
         cls._class = cls.__name__
 
 class BaseFeatureRecord(metaclass=FeatureMeta):
-    pass
+    def __init__(self):
+      self.session = Session()
+
+    def getModel(self):
+        return self.session.query(feature.Feature).filter(feature.Feature.name == self._class).one()
   
--- tests/test_base_feature_record.py
+++ tests/test_base_feature_record.py
@@ -1,8 +1,23 @@
-from ml_buff.models import base_feature_record
+from ml_buff.models import base_feature_record, feature
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 class TestFeature(base_feature_record.BaseFeatureRecord):
-  pass
+    def __init__(self):
+        self.engine = create_engine('sqlite:///:memory:')
+        feature.Base.metadata.create_all(self.engine)
+        self.feature = feature.Feature(self._class)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+        self.session.add(self.feature)
+        self.session.commit()
 
 def test_classname():
     test_feature = TestFeature()
     assert test_feature._class == 'TestFeature'
+
+def test_getModel():
+    test_feature = TestFeature()
+    result = test_feature.getModel()
+    assert result.name == 'TestFeature'
