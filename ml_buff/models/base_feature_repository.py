4c2411e454ac870fe6756df49583655c94218d32
Bruno Tinen - Prestador <brunotinen.fdte@ikeda.local>
2017-03-07 19:59:29 -0300

    Get last value of a feature [WIP]

--- ml_buff/models/base_feature_repository.py
+++ ml_buff/models/base_feature_repository.py
@@ -1,13 +1,14 @@
-from . import feature
+from feature import Feature
+from feature_value import FeatureValue
 
 class BaseFeatureRepository():
 	def create(self, session, classname):
-		feature = feature.Feature(classname)
+		feature = Feature(classname)
 		session.add(self.feature)
 
 	def get(self, session, classname):
-		return session.query(feature.Feature).filter(feature.Feature.name == classname).one_or_none()
+		return session.query(Feature).filter(Feature.name == classname).one_or_none()
 
 	def getValue(self, session, classname):
-        model = self.get(session, classname)
+        model = session.query(Feature).options(subqueryload(Feature.feature_values)).filter(Feature.name == classname).order_by(FeatureValue.created_at).first()
         return model.feature_values[0]
